336a6bc0bf8158f527de27b8176b4ebf160ffc36
nitu <nitu8860@gmail.com>
2023-02-12 18:13:32 +0530

    Initial version

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO student (student_id, first_name, last_name, age, department) VALUES (1, 'John', 'Sharma', 25, 'Computer Science');
+INSERT INTO student (student_id, first_name, last_name, age, department) VALUES (2, 'Nia', 'Doe', 22, 'Mathematics');
+INSERT INTO student (student_id, first_name, last_name, age, department) VALUES (3, 'Janny', 'Smith', 23, 'Electronics');
+INSERT INTO student (student_id, first_name, last_name, age, department) VALUES (4, 'Aman', 'Kumar', 24, 'Biology');
+INSERT INTO student (student_id, first_name, last_name, age, department) VALUES (5, 'Aliya', 'Williams', 26, 'Physics');
+INSERT INTO event (event_id, event_name, location_of_event, date, start_time, end_time, imageurl) VALUES (1, 'Concert', 'Madison Square Garden', '2022-12-25', '19:00', '22:00', 'https://via.placeholder.com/600/92c952');
+INSERT INTO event (event_id, event_name, location_of_event, date, start_time, end_time, imageurl) VALUES (2, 'Festival', 'Central Park', '2022-07-04', '12:00', '23:00', 'https://via.placeholder.com/600/92c952');
+INSERT INTO event (event_id, event_name, location_of_event, date, start_time, end_time, imageurl) VALUES (3, 'Exhibition', 'Metropolitan Museum of Art', '2022-11-01', '10:00', '17:00', 'https://via.placeholder.com/600/92c952');
